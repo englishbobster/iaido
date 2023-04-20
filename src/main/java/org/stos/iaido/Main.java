0e59434a6081d159fdee5bfe415bb6668fe7abab
Stu <stuart.osborne@nordnet.se>
2023-04-20 20:03:35 +0200

    Initial commit.

---
+++ src/main/java/org/stos/iaido/Main.java
@@ -0,0 +1,5 @@
+package org.stos.iaido;
+
+public class Main {
+    public static void main(String[] args) {}
+}
---
+++ src/test/java/org/stos/iaido/SomeTest.java
@@ -0,0 +1,14 @@
+package org.stos.iaido;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class SomeTest {
+
+    @Test
+    void someTest() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
