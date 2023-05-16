1e7e043c9c15922e64946bc04eebd6d0bdbf3a13
Stu <stuart.osborne@nordnet.se>
2023-05-16 22:24:38 +0200

    Added back propagation test for single perceptron.

--- src/test/java/org/stos/iaido/BackPropagatorTest.java
+++ src/test/java/org/stos/iaido/BackPropagatorTest.java
@@ -3,6 +3,7 @@ package org.stos.iaido;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
 public class BackPropagatorTest {
 
@@ -31,5 +32,41 @@ public class BackPropagatorTest {
         assertThat(e.getGrad()).isEqualTo(-2.0);
         assertThat(f.getGrad()).isEqualTo(4.0);
         assertThat(L.getGrad()).isEqualTo(1.0);
+    }
+
+    @Test
+    void shouldBackPropagateOnPerceptron() {
+        //a perceptron with 2 inputs (2D)
+        //inputs x0 and x1
+        CalcNode
+                x0 = new CalcNode(2.0, "x0"),
+                x1 = new CalcNode(0.0, "x1");
+        //weights w0 and w1
+        CalcNode
+                w0 = new CalcNode(-3.0, "w0"),
+                w1 = new CalcNode(1.0, "w1");
+        //neuron bias
+        CalcNode bias = new CalcNode(6.8813735870, "bias"); //adjusted according to karpathy video
+
+        //sum of weighted inputs and bias
+        CalcNode x0w0 = x0.multiply(w0); x0w0.setLabel("x0w0");
+        CalcNode x1w1 = x1.multiply(w1); x1w1.setLabel("x1w1");
+        CalcNode sumInputs = x0w0.add(x1w1); sumInputs.setLabel("summed inputs");
+        CalcNode addBias = sumInputs.add(bias); addBias.setLabel("n");
+
+        CalcNode output = addBias.tanh(); output.setLabel("output");
+        new BackPropagator().backProp(output);
+
+        assertThat(x0.getGrad()).isCloseTo(-1.5, offset(0.001));
+        assertThat(w0.getGrad()).isCloseTo(1, offset(0.001));
+        assertThat(x1.getGrad()).isCloseTo(0.5, offset(0.001));
+        assertThat(w1.getGrad()).isEqualTo(0);
+        assertThat(x0w0.getGrad()).isCloseTo(0.5, offset(0.001));
+        assertThat(x1w1.getGrad()).isCloseTo(0.5, offset(0.001));
+        assertThat(sumInputs.getGrad()).isCloseTo(0.5, offset(0.001));
+        assertThat(bias.getGrad()).isCloseTo(0.5, offset(0.001));
+        assertThat(addBias.getGrad()).isCloseTo(0.5, offset(0.001));
+        assertThat(output.getGrad()).isEqualTo(1);
+
     }
 }
