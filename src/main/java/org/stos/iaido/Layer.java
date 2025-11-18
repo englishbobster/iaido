090687dcfe4052dff31709d3a9ae5354cf5d4d12
Stu <stuart.osborne@nordnet.se>
2025-11-18 23:08:45 +0100

    Started to construct MLP...missed files

---
+++ src/main/java/org/stos/iaido/Layer.java
@@ -0,0 +1,28 @@
+package org.stos.iaido;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+public class Layer {
+
+    private List<Neuron> neurons;
+
+    public Layer(int numberOfNeuronsIn, int numberOfNeuronsOut) {
+        initializeLayer(numberOfNeuronsIn, numberOfNeuronsOut);
+    }
+
+    private void initializeLayer(int in, int out) {
+        List<Neuron> neurons = new ArrayList<>();
+        IntStream.range(0, out).forEach(i -> neurons.add(new Neuron(in)));
+        this.neurons = neurons;
+    }
+
+    public Function<List<Double>, List<CalcNode>> prepareLayer() {
+        return doubles -> IntStream.range(0, neurons.size())
+                .boxed()
+                .map(i -> neurons.get(i).wireNeuron().apply(doubles))
+                .toList();
+    }
+}
---
+++ src/main/java/org/stos/iaido/Neuron.java
@@ -0,0 +1,45 @@
+package org.stos.iaido;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+public class Neuron {
+    private List<CalcNode> weights;
+    private CalcNode bias;
+    private final Random random;
+
+    public Neuron(int numberOfInputs) {
+        random = new Random();
+        initializeWeights(numberOfInputs);
+        initializeBias();
+    }
+
+    private void initializeBias() {
+        bias = new CalcNode(random.nextDouble(-1.0, 1.0), "b");
+    }
+
+    private void initializeWeights(int numberOfInputs) {
+        List<CalcNode> nodes = new ArrayList<>();
+        IntStream.range(0, numberOfInputs)
+                .forEach(i -> nodes.add(
+                                new CalcNode(random.nextDouble(-1.0, 1.0), "w" + i)
+                        )
+                );
+        weights = nodes;
+    }
+
+    public Function<List<Double>, CalcNode> wireNeuron() {
+        return doubles -> {
+            int size = Math.min(weights.size(), doubles.size());
+            CalcNode activation = IntStream.range(0, size).boxed()
+                    .map(i -> weights.get(i).multiply(doubles.get(i)))
+                    .reduce(CalcNode::add).orElseThrow().add(bias);
+            return activation.tanh();
+        };
+    }
+
+
+}
